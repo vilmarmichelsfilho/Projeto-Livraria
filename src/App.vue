b8f8f66ae3eb54ee28f5ddda2cbd86533f35c0cb
Igor Gabriel Decker <igorgabrieldecker201@gmail.com>
2026-06-05 20:02:03 -0300

    FEAT005-Criação footer Fixes #7

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import appFooter from './components/layout/appFooter.vue';
 </script>
 
 <template>
-  <RouterView />
+  <div class="app-container">
+     <RouterView/>
+     <appFooter></appFooter>
+  </div>
 </template>
 <style scoped>
+div.app-container {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+}
 </style>
